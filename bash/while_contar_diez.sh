71b66957269c7ad7f8606f0f33d9305024a6a8f6
willoom <137672356+willoom@users.noreply.github.com>
2024-12-18 16:21:09 +0100

    Update while_contar_diez.sh

--- bash/while_contar_diez.sh
+++ bash/while_contar_diez.sh
@@ -12,12 +12,12 @@ contador=0
 # Número de iteración
 vuelta=1
 
-while [ vuelta -le 10 ]; do
+while [ $vuelta -le 10 ]; do
   # acumulamos el valor actual de iteración
-  contador=$( $contador + $vuelta )
+  contador=$(($contador + $vuelta))
 
   # incrementamos el iterador para garantizart que salimos del bucle en diez iteraciones.
-  vuelta=$( $vuelta + 1 )
+  vuelta=$(($vuelta + 1))
 done
 
 echo "La suma de los diez primeros naturales es $contador"
