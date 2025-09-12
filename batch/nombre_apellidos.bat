8de0b27c7900caf5af0af164839b347ca034da77
willoom <137672356+willoom@users.noreply.github.com>
2025-09-12 11:44:04 +0200

    Update nombre_apellidos.bat
    Comentarios

--- batch/nombre_apellidos.bat
+++ batch/nombre_apellidos.bat
@@ -1,5 +1,14 @@
 @echo off
 
+REM Este script recibe como parámetros APELLIDO1 APELLIDO2 NOMBRE.
+REM Asigna estos parámetros respectivamente a las variables %1, %2, %3.
+REM Muestra por pantalla el significado.
+REM Tratamos una variable que contiene una ruta a un fichero: %0.
+REM %0 es el archivo del propio script.
+
+REM Fecha: 12 de septiembre de 2024 
+REM - Willoom
+
 echo Nombre: %3
 echo Apellidos: %1 %2
 
@@ -8,4 +17,5 @@ echo letra de unidad: %~d0%
 echo path sin unidad: %~p0%
 echo nombre sin extensión: %~n0%
 echo extensión: %~x0%
+
 echo todo: %~0%
