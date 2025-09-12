8d64fa1a0861f8e1c1860f067ddb5b29012c0aae
willoom <137672356+willoom@users.noreply.github.com>
2025-09-12 11:53:18 +0200

    Update saltito.bat
    Corrección de comentarios.

--- batch/saltito.bat
+++ batch/saltito.bat
@@ -1,10 +1,14 @@
 @echo off
 
 REM Este script recibe un parámetro. 
-REM Si éste parámetro es la cadena vacía saltamos a ERROR y se muestra "..." por pantalla,
+
+REM Si éste parámetro es la cadena vacía, la ejecución salta a ERROR y 
+REM se muestra "Falta el primer parámetro" por pantalla,
 REM el script luego ejecuta la sección etiquetada como FIN y el script finaliza.
-REM Si el parámetro no es la cadena vacía la ejecución salta directamente a la sección FIN,
-REM no se imprime "..." y el script finaliza.
+
+REM Si el parámetro no es la cadena vacía,
+REM se imprime "...",
+REM la ejecución salta directamente a la sección FIN y el script finaliza.
 
 REM Fecha: 12 de septiembre de 2024 
 REM - Willoom
@@ -18,4 +22,5 @@ echo Falta el primer par metro
 
 :FIN
 exit /b
+
 
