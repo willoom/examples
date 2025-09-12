107ac5e3a146cf92bfd40d73f6b25a813fa68097
willoom <137672356+willoom@users.noreply.github.com>
2025-09-12 11:39:19 +0200

    Update for.bat
    Comentario

--- batch/for.bat
+++ batch/for.bat
@@ -1,3 +1,10 @@
 @echo off
 
+REM Este script itera sobre los números del 1 al 6 con un for. 
+REM En cada iteración, la variable a va recibiendo un valor y
+REM se imprime por pantalla este valor.
+
+REM Fecha: 12 de septiembre de 2024 
+REM - Willoom
+
 for %%a in (1 2 3 4 5 6) do @echo %%a
