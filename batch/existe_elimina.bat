ec7f5bcc726dba9b68b97e5cc2f0f604a8acda6d
willoom <137672356+willoom@users.noreply.github.com>
2025-09-12 11:37:34 +0200

    Update existe_elimina.bat
    Comentarios

--- batch/existe_elimina.bat
+++ batch/existe_elimina.bat
@@ -1,7 +1,15 @@
 @echo off
+
+REM Este script comprueba si existe el fichero token.txt. 
+REM Si es así lo borra. En otro caso indica que no existe.
+
+REM Fecha: 12 de septiembre de 2024 
+REM - Willoom
+
 if exist token.txt (
 	echo borrando token.txt
 	del token.txt
 ) else (
 	echo El fichero token.txt no existe
 )
+
