cae9a47be51d27e6dd1cf5a5a1a2ce58839acb2d
willoom <137672356+willoom@users.noreply.github.com>
2025-09-12 11:35:58 +0200

    Update definida.bat
    Comentarios

--- batch/definida.bat
+++ batch/definida.bat
@@ -1,4 +1,12 @@
 @echo off
+
+REM Este script define la variable a. 
+REM Comprueba si las variables a y b están definidas.
+
+REM Fecha: 12 de septiembre de 2024 
+REM - Willoom
+
 set a=1
 if DEFINED a ( echo Variable a definida ) else ( echo Variable a no definida )
+
 if DEFINED b ( echo Variable b definida ) else ( echo Variable b no definida )
