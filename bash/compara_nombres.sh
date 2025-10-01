00c087f83bf63fddcbaa784719d5c374c60291c3
willoom <137672356+willoom@users.noreply.github.com>
2025-10-01 16:49:39 +0200

    Create compara_nombres.sh

---
+++ bash/compara_nombres.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Introduce un nombre" nombre
+
+if test "$nombre" == "Juan"
+then
+  echo "¡Hola Juanito!"
+elif test "$nombre" == "Pedro"
+then
+  echo "¡Hola Pedrito!"
+else
+  echo "No te conozco."
+fi
